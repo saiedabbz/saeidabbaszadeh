9d690aecb5d0d514b9a1f1814501180101a2d857
saeidabz <saeidabbaszadeh.91@gmail.com>
2022-08-09 19:54:48 +0430

    form edit & new desing

--- contact/views.py
+++ contact/views.py
@@ -6,22 +6,22 @@ from product.models import Product
 
 
 def InQuery(request, slug):
-    products = Product.objects.get(slug=slug)
+    product = Product.objects.get(slug=slug)
 
     if request.method == "POST":
-        product = request.POST.get('product')
+        # product = request.POST.get('product')
         company_name = request.POST.get('company_name')
         first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
+        # last_name = request.POST.get('last_name')
         email = request.POST.get('email')
         phone = request.POST.get('phone')
         quantity = request.POST.get('quantity')
         description = request.POST.get('description')
 
-        prod = Product.objects.get(pk=product)
+        # prod = Product.objects.get(pk=product)
 
-        contact = Contact(product=prod, company_name=company_name, first_name=first_name, last_name=last_name, email=email, phone=phone, quantity=quantity, description=description )
+        contact = Contact(product=product, company_name=company_name, first_name=first_name, email=email, phone=phone, quantity=quantity, description=description )
         contact.save()
         return redirect("homepage")
 
-    return render(request, 'add_inquery.html', {'products': products,})
+    return render(request, 'add_inquery.html', {'product': product,})
